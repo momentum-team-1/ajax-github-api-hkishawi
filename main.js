fba54a42d82371eaf3c3434070076ddb2bc5f9c4
hkishawi <hkishawi@gmail.com>
2020-05-07 14:09:06 -0400

    got text and image

--- main.js
+++ main.js
@@ -1,36 +1,91 @@
-    let dataSection = document.querySelector("#githubdata")
+let dataSection = document.querySelector(".githubdata")
 
-    fetch('https://api.github.com/users/hkishawi')
-    //promise object represents eventual completion/failure of asynchornous operation and its resulting value. fetch is getting a promise
+    function createImageElement(url) {
+                return `<img src="${url}">`
+    }
+
+fetch('https://api.github.com/users/hkishawi')
     .then(function(response) {
-        //^we are accessing data from the response 
         console.log("response")
         return response.json()
     })
-    .then(function(data) {
-        //data from json can be used
-        console.log(data)
-        const divEl = document.createElement('li')
-        divEl.innerText = data.name
-        dataSection.appendChild(divEl)
-        return data.repos_url
+
+    .then(function(response) {
+        //data from json can be used as response
+        const aboutMe = document.createElement("section")
+        aboutMe.className = "about-me"
+        aboutMe.innerText = ("About Me")
+        dataSection.appendChild(aboutMe)
+
+        const firstLi = document.createElement("div")
+        firstLi.className = "name"
+        firstLi.innerText = ("name: " + response.name)
+        aboutMe.appendChild(firstLi)
+        
+        const  secLi = document.createElement("div")
+        secLi.className = "location"
+        secLi.innerText = ("located in: " + response.location)
+        aboutMe.appendChild(secLi)
+
+        const thirdLi = document.createElement("div")
+        thirdLi.className = "email"
+        thirdLi.innerText = ("email: " + response.blog)
+        aboutMe.appendChild(thirdLi)
+
+       const profilePic = document.createElement("div");
+       profilePic.className = "profile-pic"
+       profilePic.innerHTML = createImageElement(response.avatar_url)
+       aboutMe.appendChild(profilePic);
+       console.log("picture")
     })
-    .then(reposUrl => fetch(reposUrl))
-    //this is same as .then(function(reposUrl)
-    //return fetch(reposUrl)
-    .then(response => response.json())
-    .then(function (reposData) {
-        console.log(reposData)
-        const repoList = document.createElement("ul")
-        repoList.classList.add(
-            ""
-        )
-        dataSelection.appendChild(repoList)
-        for (let repo of reposData) {
-            console.log(repo.full_name)
-            const listItem = document.createElement('li')
-            listItem.textContent = repo.full_name
-            listItem.classList.add("")
-            repoList.appendChild(listItem)
-        }
-    })
+
+    .then(function(header)
+    // function createImageSection (response) {
+    //     const avatar = new iamge 
+    //     avatar.className = "avatar"
+    //     avatar.src = response.avatar_urldataSection.appendChild(avatar)
+    // }
+    //^this is not working yet..
+
+        // console.log(response)
+        // const divEl = document.createElement('li')
+        // divEl.className = "name"
+        // divEl.innerText = response.name
+
+
+
+
+
+    //     divEl.innerText = data.name
+    //     dataSection.appendChild(divEl)
+    //     return data.repos_url
+
+    //     divEl.innerText = data.company
+    // })
+
+    // .then(function(company) {
+    //     console.log("company")
+    //     const divEl = document.createElement('li')
+    //     divEl.innerText = data.company
+    //     dataSection.appendChild(divEl)
+    //     company.classList.add('company')
+    // })
+    // .then(reposUrl => fetch(reposUrl))
+    // //this is same as .then(function(reposUrl)
+    // //return fetch(reposUrl)
+    // .then(response => response.json())
+    // .then(function (reposData) {
+    //     console.log(reposData)
+    //     const repoList = document.createElement("ul")
+    //     repoList.classList.add(
+    //         ""
+    //     )
+    //     dataSelection.appendChild(repoList)
+    //     for (let repo of reposData) {
+    //         console.log(repo.full_name)
+    //         const listItem = document.createElement('li')
+    //         listItem.textContent = repo.full_name
+    //         listItem.classList.add("")
+    //         repoList.appendChild(listItem)
+    //     }
+    // })
